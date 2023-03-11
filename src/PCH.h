8c454e30cb81509bf2e845aca9273d733f7b0d18
mlthelama <47679057+mlthelama@users.noreply.github.com>
2023-03-11 04:13:04 +0100

    `update`
    * minor update so it works with the newer vcpkg installations
    * general reformat of the code

--- src/PCH.h
+++ src/PCH.h
@@ -1,28 +1,19 @@
 #pragma once
 
-#include "RE/Skyrim.h"
-#include "SKSE/SKSE.h"
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
 
-#pragma warning(push)
 #include <spdlog/sinks/basic_file_sink.h>
-#ifdef NDEBUG
-#include <spdlog/sinks/basic_file_sink.h>
-#else
-#include <spdlog/sinks/msvc_sink.h>
-#endif
-#pragma warning(pop)
 
 using namespace std::literals;
 
-namespace stl {
-    using namespace SKSE::stl;
-
-    constexpr std::string_view safe_string(const char* a_str) { return a_str ? a_str : ""sv; }
+namespace stl
+{
+	using namespace SKSE::stl;
 }
 
 namespace logger = SKSE::log;
 
-#define DLLEXPORT __declspec(dllexport)
 #define EXTERN_C extern "C"
 
 #include "Version.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,83 @@
-﻿void init_logger() {
-    if (static bool initialized = false; !initialized) {
-        initialized = true;
-    } else {
-        return;
-    }
+﻿void init_logger()
+{
+	if (static bool initialized = false; !initialized) {
+		initialized = true;
+	} else {
+		return;
+	}
 
-    try {
-#ifndef NDEBUG
-        auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-        auto path = logger::log_directory();
-        if (!path) {
-            stl::report_and_fail("failed to get standard log path"sv);
-        }
+	try {
+		auto path = logger::log_directory();
+		if (!path) {
+			stl::report_and_fail("failed to get standard log path"sv);
+		}
 
-        *path /= fmt::format("{}.log"sv, Version::PROJECT);
-        auto sink = make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
-        auto log = make_shared<spdlog::logger>("global log"s, move(sink));
+		*path /= fmt::format("{}.log"sv, Version::PROJECT);
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 
-#ifndef NDEBUG
-        log->set_level(spdlog::level::trace);
-#else
-        log->set_level(spdlog::level::trace);
-        log->flush_on(spdlog::level::trace);
-#endif
+		log->set_level(spdlog::level::info);
+		log->flush_on(spdlog::level::info);
 
-        set_default_logger(move(log));
-        spdlog::set_pattern("[%H:%M:%S.%f] %s(%#) [%^%l%$] %v"s);
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("[%H:%M:%S.%f][%s(%#)][%!][%l] %v"s);
 
-        logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
-        
-    } catch (const std::exception& e) { logger::critical("failed, cause {}"sv, e.what()); }
+		logger::info("{} v{}"sv, Version::PROJECT, Version::NAME);
+
+	} catch (const std::exception& e) {
+		logger::critical("failed, cause {}"sv, e.what());
+	}
 }
 
-EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
-#ifndef NDEBUG
-    while (!IsDebuggerPresent()) {}
-#endif
-    
-    init_logger();
+EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	init_logger();
 
-    logger::info("{} loading"sv, Version::PROJECT);
+	logger::info("{} loading"sv, Version::PROJECT);
 
-    Init(a_skse);
+	Init(a_skse);
 
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
-        switch (a_msg->type) {
-            case SKSE::MessagingInterface::kDataLoaded:
-                logger::info("Data loaded"sv);
+	SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
+		switch (a_msg->type) {
+		case SKSE::MessagingInterface::kDataLoaded:
+			logger::info("Data loaded"sv);
 
-                logger::info("Done with adding"sv);
-                break;
-        }
-    });
+			logger::info("Done with adding"sv);
+			break;
+		}
+	});
 
-    logger::info("{} loaded"sv, Version::PROJECT);
-    return true;
+	logger::info("{} loaded"sv, Version::PROJECT);
+	return true;
 }
 
 EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Version = []() noexcept {
-    SKSE::PluginVersionData v;
-    v.PluginName(Version::PROJECT.data());
-    v.AuthorName(Version::AUTHOR);
-    v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH, Version::BETA });
-    v.UsesAddressLibrary(true);
-    v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
-    return v;
+	SKSE::PluginVersionData v;
+	v.PluginName(Version::PROJECT.data());
+	v.AuthorName(Version::AUTHOR);
+	v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH, Version::BETA });
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
+	return v;
 }();
 
 EXTERN_C [[maybe_unused]] __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse,
-    SKSE::PluginInfo* pluginInfo) {
-    pluginInfo->name = SKSEPlugin_Version.pluginName;
-    pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
-    pluginInfo->version = SKSEPlugin_Version.pluginVersion;
+	SKSE::PluginInfo* pluginInfo)
+{
+	pluginInfo->name = SKSEPlugin_Version.pluginName;
+	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
+	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
 
+	if (a_skse->IsEditor()) {
+		logger::critical("Loaded in editor, marking as incompatible"sv);
+		return false;
+	}
 
-    if (a_skse->IsEditor()) {
-        logger::critical("Loaded in editor, marking as incompatible"sv);
-        return false;
-    }
+	const auto ver = a_skse->RuntimeVersion();
+	if (ver < SKSE::RUNTIME_SSE_1_5_39) {
+		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
+		return false;
+	}
 
-    const auto ver = a_skse->RuntimeVersion();
-    if (ver < SKSE::RUNTIME_SSE_1_5_39) {
-        logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
-        return false;
-    }
-
-    return true;
+	return true;
 }
